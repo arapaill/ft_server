3fea0549a848b0f6575f2e60b075e78a184333a8
arapaill <arapaill@student.s19.be>
2021-01-14 18:32:22 +0100

    working fine

--- dockerfile
+++ dockerfile
@@ -2,24 +2,17 @@ From		debian:buster
 
 LABEL		Arapaill <arapaill@student.s19.fr>
 
-RUN			apt-get update
-RUN			apt-get upgrade -y
-RUN			apt-get -y install wget
-RUN			apt-get -y install nginx
-RUN			apt-get -y install mariadb-server
-RUN			apt-get -y install php7.3 php-mysql php-fpm php-pdo php-gd php-cli php-mbstring
-RUN			wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-english.tar.gz
-RUN			tar -xf phpMyAdmin-5.0.1-english.tar.gz && rm -rf phpMyAdmin-5.0.1-english.tar.gz
-RUN			mv phpMyAdmin-5.0.1-english phpmyadmin
-RUN			wget https://wordpress.org/latest.tar.gz
-RUN			tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
-RUN 		openssl req -x509 -nodes -days 365 -subj "/C=BE/ST=Belgium/L=Bruxelles/O=innoaca/OU=19Bruxelles/CN=arapaill" -newkey rsa:2048 -keyout /etc/ssl/nginx-selfsigned.key -out /etc/ssl/nginx-selfsigned.crt;
-RUN 		chown -R www-data.www-data /var/www/
-RUN 		chmod -R 755 /var/www/*
+RUN apt-get update && \
+apt-get install -y nginx && \
+apt-get install -y wget && \
+apt-get install -y curl && \
+apt-get install -y openssl && \
+apt-get install -y mariadb-server mariadb-client && \
+apt-get install -y php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline && \
+apt-get install -y php-mbstring php-zip php-gd && \
+apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
 
-COPY		./src/default.conf .
-COPY		./src/init.sh ./
-COPY		./src/config.inc.php phpmyadmin
-COPY		./src/wp-config.php /var/www/html
-
-CMD			bash init.sh
+COPY ./src/default.conf .
+COPY ./src/init.sh .
+COPY ./src/wp-config.php .
+COPY ./src/config.inc.php .
--- src/init.sh
+++ src/init.sh
@@ -1,11 +1,47 @@
-service nginx start
+
+curl -L https://github.com/FiloSottile/mkcert/releases/download/v1.1.2/mkcert-v1.1.2-linux-amd64 > mkcert
+chmod +x mkcert
+
+./mkcert -install
+./mkcert localhost
+
+mkdir /etc/nginx/ssl
+mv ./localhost* /etc/nginx/ssl/
+
+# NGINX
+
+mkdir var/www/localhost
+mv ./default.conf etc/nginx/sites-available
+ln -sf /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled
+
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
+
+# MSQL
 service mysql start
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+# phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+tar xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
+mv phpMyAdmin-4.9.0.1-all-languages var/www/localhost/phpmyadmin
+mv ./config.inc.php var/www/localhost/phpmyadmin
+chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
+chown -R www-data:www-data /var/www/localhost/phpmyadmin
 service php7.3-fpm start
 
-# Configure a wordpress database
-echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
-echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY 'pmapass';" | mysql -u root
+echo "FLUSH PRIVILEGES;" mysql -u root
 
-bash
+#WORDPRESS
+wget https://wordpress.org/latest.tar.gz
+tar xvf latest.tar.gz
+mkdir var/www/localhost/wordpress
+cp -a wordpress/. /var/www/localhost/wordpress
+mv ./wp-config.php /var/www/localhost/wordpress
+
+service nginx start
+service php7.3-fpm restart
+service mysql restart
